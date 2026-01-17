8f8c64a4f92cff3e15806b1a10a79f5318c3960e
komal-kumawat <kumawatkomal17122004@gmail.com>
2026-01-18 01:24:17 +0530

    hero section done

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,11 +12,13 @@ const Button = ({ icon, name , url }: ButtonProps) => {
     window.open(url, "_blank"); // opens in new tab
   };
   return (
-    <div className="inline-flex items-center gap-1 align-middle border border-dotted rounded-md px-2 py-[2px] hover:border-gray-400 cursor-pointer"
+    <div className="inline-flex items-center gap-1 align-middle border border-dotted rounded-md px-2 py-[2px] hover:border-gray-400 cursor-pointer border border-neutral-300 dark:border-neutral-700
+      bg-white dark:bg-neutral-900
+      text-neutral-900 dark:text-gray-200"
     onClick={handleClick}
     >
         {icon}
-      <span className="text-[0.9em] hover:text-gray-400">{name}</span>
+      <span className="text-[0.9em] hover:text-gray-400 text-gray-700 dark:text-gray-400">{name}</span>
     </div>
   );
 };
